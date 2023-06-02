e56733645f06c83870ae33108167d05bdd42b2e4
tanveerisonline <tanveerisonline@gmail.com>
2023-06-03 00:18:39 +0530

    Setup react project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Greeting from './components/Greeting';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/greeting" element={<Greeting />} />
+    </Routes>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import { Provider } from 'react-redux';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import store from './redux/configureStore';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+);
+
+reportWebVitals();

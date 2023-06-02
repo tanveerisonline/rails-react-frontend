c87c8514cb0e34c035565e6bd4b648f823cfea4f
tanveerisonline <tanveerisonline@gmail.com>
2023-06-03 00:22:52 +0530

    Create Greeting and Home components

---
+++ src/components/Greeting.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { getRandomGreeting } from '../redux/greetings';
+
+function Greeting() {
+  const greeting = useSelector((state) => state.greeting);
+  const error = useSelector((state) => state.error);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getRandomGreeting());
+  }, [dispatch]);
+
+  return (
+    <section className="page">
+      <h1>Random Greeting</h1>
+      <h2>{greeting}</h2>
+      <Link to="/">
+        <button type="button">Back to Home</button>
+      </Link>
+      <small>{error}</small>
+    </section>
+  );
+}
+
+export default Greeting;
